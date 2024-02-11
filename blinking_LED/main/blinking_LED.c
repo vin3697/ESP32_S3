0ad74f557a95c567faddf0fe102b4c453ac8af12
vinayakvishwaspatil <st174693@stud.uni-stuttgart.de>
2024-02-11 18:37:33 +0100

    updated_blinking_LED

--- blinking_LED/main/blinking_LED.c
+++ blinking_LED/main/blinking_LED.c
@@ -12,22 +12,22 @@
 
 //#define CONFIG_BLINK_LED_STRIP 1
 
-static uint8_t s_led_state = 0;
 static led_strip_handle_t led_strip;
 static const char *TAG = "example";
 
-static void blink_led(void)
+
+
+static void on_led(void){
+    /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
+    led_strip_set_pixel(led_strip, 0, 255, 0, 0);
+    /* Refresh the strip to send data */
+    led_strip_refresh(led_strip);
+}
+
+static void off_led(void)
 {
-    /* If the addressable LED is enabled */
-    if (s_led_state) {
-        /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
-        led_strip_set_pixel(led_strip, 0, 255, 0, 0);
-        /* Refresh the strip to send data */
-        led_strip_refresh(led_strip);
-    } else {
-        /* Set all LED off to clear all pixels */
-        led_strip_clear(led_strip);
-    }
+    /* Set all LED off to clear all pixels */
+    led_strip_clear(led_strip);   
 }
 
 static void configure_led(void)
@@ -58,10 +58,11 @@ void app_main(void)
     configure_led();
 
     while (1) {
-        ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
-        blink_led();
+        //ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
+        on_led();
         /* Toggle the LED state */
-        s_led_state = !s_led_state;
+        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+        off_led();
         vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
     }
 }
