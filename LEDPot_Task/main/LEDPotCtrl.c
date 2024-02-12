c5d7b9aac1e9a5305cef1fcad811f73422f93e9f
vinayakvishwaspatil <st174693@stud.uni-stuttgart.de>
2024-02-12 22:51:15 +0100

    added_stwrTimer_colorChng

--- LEDPot_Task/main/LEDPotCtrl.c
+++ LEDPot_Task/main/LEDPotCtrl.c
@@ -20,7 +20,9 @@ and color change depending upon Potentiometer
 //header files LED and ADC
 #include "wr_LED.h"
 #include "read_ADC.h" 
-#define PERIOD_MS 100
+#define PERIOD_MS 10
+
+
 
 static const char *TAG   = "example";
 static int voltage   = 0;
@@ -29,24 +31,102 @@ const TickType_t xDelay  = CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS; //delay of 
 
 //declarations of functions
 static void blink_LED(void);
+
+//blinking with Freq.
 static void vLEDFreqTimerSetup();
 static void vLEDfreqTimerCallback( TimerHandle_t pxTimer );
 
+//color change w.r.t voltage
+static void vcolrChngTimerSetup();
+static void vcolrChngCallback();
+
+#define COLOR_CHANGE TRUE
+
+
 //software timer implementation
-TimerHandle_t xLEDblinkFreqTimer = NULL;                             //Timer for BlinkLED freq w.r.t voltage
+TimerHandle_t xLEDblinkFreqTimer = NULL;                             //Timer handle for BlinkLED freq w.r.t voltage
+TimerHandle_t xcolrChngTimer     = NULL;                             //Timer handle for color change  w.r.t voltage
 
-
-//main function
+/*
+                                        app_main function
+*/
 void app_main(void)
 {
     // configure the peripherals!
     configure_led();
     configure_adc();
 
-    vLEDFreqTimerSetup();
+    #ifdef COLOR_CHANGE
+        vcolrChngTimerSetup();
+    #elif
+        vLEDFreqTimerSetup();
+    #endif
 
 }
 
+static void blink_LED(void){
+
+    voltage = voltage / 89;       //normalize value between 0-11
+    //ESP_LOGI(TAG, "LED toggle");
+    ESP_LOGI(TAG, "Norm voltage   : %d", voltage);
+    if(voltage == 11){
+        on_led();
+    }
+    else{
+        //Toggle the LED state
+        on_led();
+        vTaskDelay(voltage*voltage*xDelay); // !delay less than 100ms
+        off_led();
+    }
+    
+}
+
+/*
+                             Below is LED color change software timer implementation
+*/
+
+static void vcolrChngTimerSetup(){
+
+    // Create then start the Auto-reload timer that is responsible for
+    // blinking the LED with different frequencies.
+    xcolrChngTimer = xTimerCreate( "colrChngTimer",           // Just a text name, not used by the kernel.
+                                    pdMS_TO_TICKS(PERIOD_MS),     // The timer period in milliSecs.
+                                    pdTRUE,                       // The timer is a one-shot timer.
+                                    0,                            // The id is not used by the callback so can take any value.
+                                    vcolrChngCallback         // The callback function that switches the LCD back-light off.
+                                );
+
+    if( xcolrChngTimer == NULL )
+    {
+        // The timer was not created.
+        ESP_LOGE(TAG, "Failed to create timer");
+    }
+    else
+    {
+        // Start the timer.  No block time is specified, and even if one was
+        // it would be ignored because the scheduler has not yet been
+        // started.
+        if( xTimerStart( xcolrChngTimer, 0 ) != pdPASS ) // xTimerStart(xTimer, xTicksToWait)
+        {
+            // The timer could not be set into the Active state.
+        }
+    }
+
+}
+
+static void vcolrChngCallback(){
+
+    //get voltage from ADC
+    voltage = read_adc();
+
+    //color change method
+    chngLEDcolr(&voltage);
+
+}
+
+/*
+                             Below is Blink LED Freq software timer implementation
+*/
 
 static void vLEDFreqTimerSetup(){
 
@@ -88,20 +168,4 @@ static void vLEDfreqTimerCallback( TimerHandle_t pxTimer )
 
 }
 
-static void blink_LED(void){
-
-    voltage = voltage / 89;       //normalize value between 0-11
-    //ESP_LOGI(TAG, "LED toggle");
-    ESP_LOGI(TAG, "Norm voltage   : %d", voltage);
-    if(voltage == 11){
-        on_led();
-    }
-    else{
-        //Toggle the LED state
-        on_led();
-        vTaskDelay(voltage*voltage*xDelay); // !delay less than 100ms
-        off_led();
-    }
-    
-}
 
